de7c995c3edb53667c9eb2b5d75a8d1a37b6eb1b
kadufreitas <kadufreitas95@gmail.com>
2024-11-22 15:28:22 +0000

    feat: add _redirects

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { sdk } from 'api'
 
+// 0efa34c76b35e48b9bf5bd0f0c09252b9a4b37ed
 sdk
   .authorize('code')
   .then(() => sdk.getMe())
   .then((user) => {
     console.log(user)
   })
+
+console.log('sdk', sdk)
 function App() {
   return (
     <div className="App">
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,6 @@ import SDK from '@uphold/uphold-sdk-javascript'
 
 export const sdk = new SDK({
   baseUrl: 'http://api-sandbox.uphold.com',
-  clientId: 'foo',
-  clientSecret: 'bar',
+  clientId: '9648007ecbc76bb58237b48790610331094c0763',
+  clientSecret: '3f82ec4e6aecf8b83390456291a12ca4eefdc976',
 })
